af60ccc59342a0a0e7fa7d950ed6facbe093339d
Alexandr Yakovlev <engelsovich75@yandex.ru>
2022-11-02 16:11:12 +0500

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
+        MagicBox<String> stringMagicBox = new MagicBox<>(3);
 
+        boolean sasha = stringMagicBox.add("Sasha");
+        boolean petya = stringMagicBox.add("Petya");
+        boolean kolya = stringMagicBox.add("Kolya");
+
+        System.out.println(sasha);
+        System.out.println(petya);
+        System.out.println(kolya);
+
+        System.out.println(stringMagicBox.pick());
+
+        MagicBox<Integer> intMagicBox = new MagicBox<>(3);
+
+        boolean firstNumber = intMagicBox.add(10);
+        boolean secondNumber = intMagicBox.add(20);
+//        boolean thirdNumber = intMagicBox.add(30);
+
+        System.out.println(firstNumber);
+        System.out.println(secondNumber);
+//        System.out.println(thirdNumber);
+
+        System.out.println(intMagicBox.pick());
     }
 }
